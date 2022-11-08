dfded7a6e2421248fbcc404a73ab382e18cc67b7
Izzy <izzycalabig@hotmail.com>
2022-11-08 09:09:02 -0800

    Added Edabit challenge/solution

---
+++ JS-Very-Easy-Strings/boolean-to-string-conversion.js
@@ -0,0 +1,26 @@
+// Task
+// Boolean to String Conversion
+// Create a function that takes a boolean variable flag and returns it as a string.
+
+
+
+// My Solution:
+
+// PREP
+// Parameters - ...
+// Return - ...
+// Examples - ...
+// Pseudocode - ...
+
+function boolToString(flag) {
+	if(flag == true){
+		return "true"
+	}else{
+		return "false"
+	}
+}
+
+// Question: ...
+
+// Code
+
