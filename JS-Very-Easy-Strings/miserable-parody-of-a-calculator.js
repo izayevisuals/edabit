69e97e48f6527939eb46e5ac90a58bdf336cd02f
Izzy <izzycalabig@hotmail.com>
2022-11-08 09:34:06 -0800

    Added Edabit challenge/solution

---
+++ JS-Very-Easy-Strings/miserable-parody-of-a-calculator.js
@@ -0,0 +1,22 @@
+// Task: 
+// Miserable Parody of a Calculator
+// Create a function that will handle simple math expressions. The input is an expression in the form of a string.
+
+// My Solution:
+
+// PREP
+// Parameters - ...
+// Return - ...
+// Examples - ...
+// Pseudocode - ...
+
+function calculator(str) {
+	return eval(str)
+}
+
+calculator('1 + 1')
+
+// Question: ...
+
+// Code
+
