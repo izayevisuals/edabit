7a5ebb4bd70e43bc926d23310ef68e9af5240c8c
Izzy <izzycalabig@hotmail.com>
2022-11-09 08:40:33 -0800

    Added Edabit challenge/solution

---
+++ JS-Very-Easy-Strings/basic-variable-assignment.js
@@ -0,0 +1,19 @@
+// Task: 
+// Basic Variable Assignment
+// A student learning JavaScript was trying to make a function. His code should concatenate a passed string name with string "Edabit" and store it in a variable called result. He needs your help to fix this code.
+
+// My Solution:
+
+// PREP
+// Parameters - 'name'
+// Return - 'name' + 'Edabit'
+// Examples - "MubashirEdabit"
+// Pseudocode - ...
+
+// Code
+
+function nameString(name){
+	return name + 'Edabit'
+}
+
+nameString('Izzy')
