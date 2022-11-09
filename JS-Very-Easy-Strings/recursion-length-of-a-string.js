fe2589297ce694cc2b65bdf3d0f344342e8cb189
Izzy <izzycalabig@hotmail.com>
2022-11-09 08:51:06 -0800

    Added Edabit challenge/solution

---
+++ JS-Very-Easy-Strings/recursion-length-of-a-string.js
@@ -0,0 +1,19 @@
+// Task: 
+// Recursion: Length of a String
+// Write a function that returns the length of a string. Make your function recursive.
+
+// My Solution:
+
+// PREP
+// Parameters - 'str'
+// Return - the length of a string
+// Examples - length("apple") ➞ 5
+// Pseudocode - find string length
+
+// Code
+
+function length(str) {
+	return str.length
+}
+
+length('Izzy')
