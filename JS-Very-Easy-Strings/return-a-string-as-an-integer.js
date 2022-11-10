91bb07e4ff79d62d35c43817e8b72c7a0cdd269a
Izzy <izzycalabig@hotmail.com>
2022-11-09 18:39:14 -0800

    Added Edabit challenge/solution

---
+++ JS-Very-Easy-Strings/return-a-string-as-an-integer.js
@@ -0,0 +1,19 @@
+// Task: 
+// Return a String as an Integer
+// Create a function that takes a string and returns it as an integer.
+
+// My Solution:
+
+// PREP
+// Parameters - 'str'
+// Return - return a string to an integer
+// Examples - stringInt("6") ➞ 6;
+// Pseudocode - parseInt()...?
+
+// Code
+
+function stringInt(str) {
+	return parseInt(str)
+}
+
+stringInt('4')
