5aec2722d1348132eadc895ab44ea7f5c670e0b2
Izzy <izzycalabig@hotmail.com>
2022-11-09 17:24:42 -0800

    Added Edabit challenge/solution

---
+++ JS-Very-Easy-Strings/is-the-string-empty.js
@@ -0,0 +1,24 @@
+// Task:
+// Is the String Empty?
+// Create a function that returns true if a string is empty and false otherwise.
+
+
+// My Solution:
+
+// PREP
+// Parameters - 's'
+// Return - return true if " " (empty) and false if not empty
+// Examples - isEmpty("") ➞ true; isEmpty("a") ➞ false;
+// Pseudocode - return true if " " (empty) and false if not empty
+
+// Code
+
+function isEmpty(s) {
+	if (s === ""){
+        return true
+    }else{
+        return false
+    }
+}
+
+isEmpty('')
