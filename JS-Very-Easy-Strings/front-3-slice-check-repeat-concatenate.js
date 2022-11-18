4525cc8a77ef6dc25b6e8fbac3f1d21242c5d2d8
Izzy <izzycalabig@hotmail.com>
2022-11-17 21:50:05 -0800

    Added Edabit challenge/solution

--- JS-Very-Easy-Strings/front-3-slice-check-repeat-concatenate.js
+++ JS-Very-Easy-Strings/front-3-slice-check-repeat-concatenate.js
@@ -1,6 +1,7 @@
 // Task: 
 // Front 3 - Slice Check Repeat Concatenate
-// Create a function that takes a string; we'll say that the front is the first three characters of the string. If the string length is less than three characters, the front is whatever is there. Return a new string, which is three copies of the front.
+// Create a function that takes a string; we'll say that the front is the first three characters of the string. 
+// If the string length is less than three characters, the front is whatever is there. Return a new string, which is three copies of the front.
 
 // My Solution:
 
@@ -12,8 +13,19 @@
 
 
 // Code
+
+
 function frontThree(str) {
     return str.slice(0,3).repeat(3)
 }
 
-frontThree("Cucumber") // "CucCucCuc"
+frontThree("Cucumber")
+// 'CucCucCuc'
+
+
+// function frontThree(str) {
+//     return str.repeat(3).slice(0,3)
+// }
+
+// frontThree("Cucumber")
+// //'Cuc'
