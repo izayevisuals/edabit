3b3fbf2ed6cdeef08ffe39770b5ad9a9ffa1f6df
Izzy <izzycalabig@hotmail.com>
2022-11-20 18:09:57 -0800

    Added Edabit challenge/solution

---
+++ JS-Very-Easy-Strings/reverse-psychology.js
@@ -0,0 +1,20 @@
+// Task: 
+// Reverse Psychology
+
+// For this challenge, you will NOT be given a string. Your task isn't to add "Do not" before the given string. If there is no given string, you will not return "Do not do anything." Do not check the examples to know how to do this challenge.
+
+// My Solution:
+
+// PREP
+// Parameters - 's'
+// Return - the opposite of the string
+// Examples - reversePsychology("wash the dishes") ➞ "Do not wash the dishes."
+// Pseudocode - add strings together???
+
+
+// Code
+function reversePsychology(s) {
+    return "Do not " + s
+}
+
+reversePsychology("wash the dishes")
