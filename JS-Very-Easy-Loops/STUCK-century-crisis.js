4620ce6d16b34b2d8185262e975615d502b68630
Izzy <izzycalabig@hotmail.com>
2022-12-07 20:19:25 -0800

    Added Edabit challenge, stuck tho...

---
+++ JS-Very-Easy-Loops/STUCK-century-crisis.js
@@ -0,0 +1,24 @@
+// Task: 
+// Scientists have discovered that in four decades, the world will EXPLODE! It will also take three decades to make a spaceship to travel to a new planet that can hold the entire world population.
+
+// You must calculate the number of people there will be in three decades from now.
+
+// The variable population is the world population now.
+// Assume that every month, someone gives birth to more people n.
+// Return the number of people there will be when the spaceship is complete.
+
+// My Solution:
+
+// PREP
+// Parameters - 'population', 'n'
+// Return - the number of people there will be in decasdes
+// Examples - futurePeople(256, 2) ➞ 976; futurePeople(3248, 6) ➞ 5408
+// Pseudocode - 
+
+// Question: 
+
+// Code
+
+function futurePeople(population, n) {
+	return (population + n(12))
+}
