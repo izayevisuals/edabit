dd80424c8454ba4f916dd77479fcdefbd61dd212
Izzy <izzycalabig@hotmail.com>
2022-12-07 21:07:19 -0800

    Added Edabit challenge/solution

---
+++ JS-Very-Easy-Loops/word-numbers.js
@@ -0,0 +1,40 @@
+// Task: 
+// Word Numbers!
+// Create a function that returns a number, based on the string provided.
+
+// My Solution:
+
+// PREP
+// Parameters - 's'
+// Return - return a number that is based on the string provided
+// Examples - one -> 1
+// Pseudocode - 
+
+// Question: 
+
+// Code
+
+function word(s) {
+	if(s === 'one'){
+        return 1
+    }else if(s === 'two'){
+        return 2
+    }else if(s === 'three'){
+        return 3
+    }else if(s === 'four'){
+        return 4
+    }else if(s === 'five'){
+        return 5
+    }else if(s === 'six'){
+        return 6
+    }else if(s === 'seven'){
+        return 7
+    }else if(s === 'eight'){
+        return 8
+    }else if(s === 'nine'){
+        return 9
+    }else{
+        return 'merp'
+    }
+}
+
