a7947773080219da8bb0daa6f6bc2c0fc81b09ce
Izzy <izzycalabig@hotmail.com>
2022-12-23 18:39:34 -0800

    Stuck ...

--- JS-Very-Easy-Loops/STUCK-true-ones-false-zeros.js
+++ JS-Very-Easy-Loops/STUCK-true-ones-false-zeros.js
@@ -7,7 +7,7 @@
 // Parameters - 'n'
 // Return - returns an array of booleans from a given number by iterating through the number one digit at a time and appending true into the array if the digit is 1 and false otherwise.
 // Examples - integerBoolean("100101") ➞ [true, false, false, true, false, true]; integerBoolean("10") ➞ [true, false]
-// Pseudocode - if... else...
+// Pseudocode - replace 0 w/ false, replace 1 w/ true, turn it into an array, split the words true and false
 
 
 // Code
@@ -16,5 +16,5 @@ function integerBoolean(n) {
 	let newN = n.replace(/0/g, false)
     let newNewN = newN.replace(/1/g, true)
 
-    return newNewN.toString()
+    return newNewN.split()
 }
